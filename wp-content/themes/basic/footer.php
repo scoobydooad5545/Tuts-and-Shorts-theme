4247bdb9b026c2226427f8f8e92477b972fa5bec
scoobydooad5545 <xxalyssadavis5545xx@gmail.com>
2015-10-26 19:47:09 -0700

    added links to footer

--- wp-content/themes/basic/footer.php
+++ wp-content/themes/basic/footer.php
@@ -1,5 +1,34 @@
-	<div id="footer">
-		<?php wp_footer(); ?>
+	<div class="row">
+		<div id="footer">
+			<?php wp_footer(); ?>
+			<div class="col-md-3">
+				<p class="footer-titles">Pages</p>
+				<ul class="footer-links">
+					<li><a href="">Home</a></li>
+					<li><a href="">About</a></li>
+					<li><a href="">Contact</a></li>
+					<li><a href="">Advertise with Us</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<p class="footer-titles">Categories</p>
+				<ul class="footer-links">
+					<li><a href="">Photoshop</a></li>
+					<li><a href="">Illustrator</a></li>
+					<li><a href="">HTML/CSS</a></li>
+					<li><a href="">Shortcuts</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<p class="footer-titles">Archives</p>
+				<ul class="footer-links">
+					<li><a href="">October 2015</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<p class="footer-titles">Get In Touch</p>
+			</div>
+		</div>
 	</div>
       
 </div><!-- x content -->
