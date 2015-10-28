4513db8603f738df5a38f71fd2f547ac80b9ed05
scoobydooad5545 <xxalyssadavis5545xx@gmail.com>
2015-10-28 13:57:56 -0700

    edited php for about page

--- wp-content/themes/basic/page-about.php
+++ wp-content/themes/basic/page-about.php
@@ -4,10 +4,22 @@ Template Name: About Page
 */
 ?>
 
+
+<div class="row">
+
+
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
+	<div class="col-md-3">
+		
+	</div>
+	<div class="col-md-9">
+		
   	<h1><?php the_title(); ?></h1>
  	<?php the_content(); ?>
+	</div>
+</div>
+
 	
 
   <?php endwhile; else: ?>
